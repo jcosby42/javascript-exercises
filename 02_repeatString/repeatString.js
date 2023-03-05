74ebc9dc5b558b7e5cce4c508a6c97f4166ccd67
jcosby42 <johncosby42@hotmail.com>
2023-03-05 12:00:37 -0500

    Solve string repeat problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    if (repeat < 0) return 'ERROR';
+    let total ='';
+    for (let i = 0; i < repeat; i++){
+        total += string;
+    }
+    return total;
 };
 
 // Do not edit below this line
