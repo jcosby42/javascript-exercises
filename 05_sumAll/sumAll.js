7e2f115f53b1eaf6250fa42b6787686ad198f67b
jcosby42 <johncosby42@hotmail.com>
2023-03-10 15:59:53 -0500

    Implement sum function and integer tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    let total = 0;
+    //I want to put this in a separate function 
+    let start, end;
+    if (a < b){
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+
+    for (let i = start; i <= end; i++){
+        total += i;
+    }
+    return total;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
