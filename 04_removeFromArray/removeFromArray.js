8f7b18a4d56a5bb53fabe59ee68274599d00fd9b
jcosby42 <johncosby42@hotmail.com>
2023-03-10 14:52:53 -0500

    Implement logic of removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+  
+    for (let item of toRemove){
+        if (array.indexOf(item) >= 0){
+            array.splice(array.indexOf(item), 1);
+        }
+    }
+  //console.log(array);
+  //cycle toRemove
 
+  /*for (let item of toRemove){
+    array.slice(array.indexOf(item), 1);
+  }*/
+  return array;
+  //search for item in array
+    //if you don't find it return error
+    //array.slice(index of item, 1)
+  //return array
 };
 
 // Do not edit below this line
