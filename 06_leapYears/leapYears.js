e60215e567043011429eadeee9ee3b0a69854fb6
jcosby42 <johncosby42@hotmail.com>
2023-03-11 23:17:18 -0500

    Use % to test for leap year math

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+/*determine if a year is a leap year*/
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
+    if (year % 400 === 0) return true;
+    if (year % 4 === 0 && year % 100 !== 0) return true;
+    return false;
 };
 
 // Do not edit below this line
