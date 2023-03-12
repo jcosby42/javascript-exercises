0711dc0de0a9a0d83e68310499ccf396ffa7bfac
jcosby42 <johncosby42@hotmail.com>
2023-03-12 12:03:25 -0400

    Implement conversion formula and add rounding helper function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+
+const roundToDecimal = function (number){
+  return (Math.round(number * 10)) / 10;
+}
+
+const convertToCelsius = function(fahr) {
+  return roundToDecimal(( fahr - 32) * 5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  return roundToDecimal(celc * 9/5 + 32);
 };
 
 // Do not edit below this line
