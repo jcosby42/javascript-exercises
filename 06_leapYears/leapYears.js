6fd126772667b074855a854b52969b654c46eda0
jcosby42 <johncosby42@hotmail.com>
2023-03-11 23:22:21 -0500

    Simplify test logic and repeat from a single line

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,12 @@
 /*determine if a year is a leap year*/
 const leapYears = function(year) {
-    if (!Number.isInteger(year)) return "ERROR";
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0); //
+
+    //This solution is fine and also mine
+    /*if (!Number.isInteger(year)) return "ERROR";
     if (year % 400 === 0) return true;
     if (year % 4 === 0 && year % 100 !== 0) return true;
-    return false;
+    return false;*/
 };
 
 // Do not edit below this line
